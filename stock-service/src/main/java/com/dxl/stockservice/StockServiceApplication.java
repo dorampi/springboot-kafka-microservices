f01d443612d31958b89b7cc94719c544565b1286
eriks.ejubs <eriks.ejubs@gmail.com>
2023-10-20 07:48:31 +0300

    [ASSIGNMENT]: - init order-service; - init stock-service; - init email-service; - init base-domains for common class sharing; - add Order dto; - add OrderEvent message type object;

---
+++ stock-service/src/main/java/com/dxl/stockservice/StockServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dxl.stockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockServiceApplication.class, args);
+	}
+
+}
